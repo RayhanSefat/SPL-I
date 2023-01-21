9a583deeb4a34bd67e697be316d42caa5b9a178b
Rayhan <bsse1332@iit.du.ac.bd>
2023-01-21 17:46:31 +0600

    Transfer balance feature added

--- 1332-SPL1/Regular Activities.h
+++ 1332-SPL1/Regular Activities.h
@@ -62,7 +62,7 @@ bool repeatationChecker()
     return false;
 }
 
-int takeBalanceType(){
+int getBalanceType(){
     cout << "\tWhich type of balance?" << endl;
     cout << "\t1. Cash balance" << endl;
     cout << "\t2. Running Account balance" << endl;
@@ -74,17 +74,122 @@ int takeBalanceType(){
     return balanceType;
 }
 
-void addBalance(){
-    int balanceType;
-    balanceType = takeBalanceType();
-
+float getAmount(){
     cout << "Enter amount: ";
     float amount;
     cin >> amount;
 
-    if(balanceType == 1) cashBalance += amount;
-    if(balanceType == 2) runningAccountBalance += amount;
-    if(balanceType == 3) savingAccountBalance += amount;
+    return amount;
+}
+
+void printCashBalance(){
+    cout << "Your current cash balance: " << cashBalance << endl;
+    return;
+}
+
+void printRunningAccountBalance(){
+    cout << "Your current running account balance: " << runningAccountBalance << endl;
+    return;
+}
+
+void printSavingAccountBalance(){
+    cout << "Your current saving account balance: " << savingAccountBalance << endl;
+    return;
+}
+
+void addBalance(){
+    int balanceType;
+    balanceType = getBalanceType();
+
+    float amount = getAmount();
+
+    if(balanceType == 1){
+        cashBalance += amount;
+        printCashBalance();
+    }
+
+    if(balanceType == 2){
+        runningAccountBalance += amount;
+        printRunningAccountBalance();
+    }
+
+    if(balanceType == 3){
+        savingAccountBalance += amount;
+        printSavingAccountBalance();
+    }
+
+    return;
+}
+
+int getTransferType(){
+    cout << "Transfer options: " << endl;
+    cout << "\t1. Running account balance to cash balance" << endl;
+    cout << "\t2. Cash balance to saving account balance" << endl;
+    cout << "\t3. Running account balance to saving balance" << endl;
+    cout << "\t4. Saving account balance to cash balance" << endl;
+
+    int transferType;
+    cin >> transferType;
+    return transferType;
+}
+
+void transferBalance(){
+    int transferType;
+    transferType = getTransferType();
+
+    float amount = getAmount();
+
+    if(transferType == 1){
+        if(amount > runningAccountBalance){
+            cout << "Sorry! You don't have sufficient balance in your running account." << endl;
+            return;
+        }
+
+        runningAccountBalance -= amount;
+        cashBalance += amount;
+
+        printRunningAccountBalance();
+        printCashBalance();
+    }
+
+    if(transferType == 2){
+        if(amount > cashBalance){
+            cout << "Sorry! You don't have sufficient balance in cash." << endl;
+            return;
+        }
+
+        cashBalance -= amount;
+        savingAccountBalance += amount;
+
+        printCashBalance();
+        printSavingAccountBalance();
+    }
+
+    if(transferType == 3){
+        if(amount > runningAccountBalance){
+            cout << "Sorry! You don't have sufficient balance in your running account." << endl;
+            return;
+        }
+
+        runningAccountBalance -= amount;
+        savingAccountBalance += amount;
+
+        printRunningAccountBalance();
+        printSavingAccountBalance();
+    }
+
+    if(transferType == 4){
+        if(amount > savingAccountBalance){
+            cout << "Sorry! You don't have sufficient balance in your saving account." << endl;
+            return;
+        }
+
+        savingAccountBalance -= amount;
+        cashBalance += amount;
+
+        printSavingAccountBalance();
+        printCashBalance();
+    }
 
     return;
 }
@@ -103,12 +208,14 @@ void activities(){
 
         cout << "What do you want to do?" << endl;
         cout << "\t1. Add your balance" << endl;
+        cout << "\t2. Transfer balance" << endl;
 
         cout << "Your response: ";
         int operationType;
         cin >> operationType;
 
         if(operationType == 1) addBalance();
+        else if(operationType == 2) transferBalance();
     }
 
     updateTheBalanceInformationSheet();
